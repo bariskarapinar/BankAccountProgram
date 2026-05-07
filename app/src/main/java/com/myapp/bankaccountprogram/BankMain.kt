f8f6c9c57f034931b2d2fa6cb66d916e0f78b5c1
bariskarapinar <bkarapinar1@hotmail.com>
2026-05-07 16:39:49 +0300

    Project Update

--- app/src/main/java/com/myapp/bankaccountprogram/BankMain.kt
+++ app/src/main/java/com/myapp/bankaccountprogram/BankMain.kt
@@ -1,17 +1,20 @@
 package com.myapp.bankaccountprogram
 
+// This is the entry point for running the code as a simple Kotlin program
 fun main(){
 
+    // Create a new bank account
     val johnsBankAccount = BankAccount("John Doe", 1338.20)
 
+    // Perform some transactions
     johnsBankAccount.deposit(200.0)
     johnsBankAccount.withdraw(1200.00)
     johnsBankAccount.deposit(3000.00)
     johnsBankAccount.deposit(2000.00)
     johnsBankAccount.withdraw(3333.15)
 
+    // Show the results in the console
     johnsBankAccount.displayTransactionHistory()
     println("${johnsBankAccount.accountHolder}'s " +
             "balance is ${johnsBankAccount.balance}")
-
 }
