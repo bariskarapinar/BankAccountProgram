c71b4f595c7056a828331f4d19e32d97bdd2f26d
bariskarapinar <bkarapinar1@hotmail.com>
2026-05-15 14:12:40 +0300

    BankAccount.kt Update

--- app/src/main/java/com/myapp/bankaccountprogram/BankAccount.kt
+++ app/src/main/java/com/myapp/bankaccountprogram/BankAccount.kt
@@ -1,27 +1,57 @@
 package com.myapp.bankaccountprogram
 
 import java.util.Locale
+import java.text.SimpleDateFormat
+import java.util.Date
+
+// Represents the type of transaction
+enum class TransactionType {
+    DEPOSIT, WITHDRAWAL
+}
+
+// Data class to store transaction details
+data class Transaction(
+    val type: TransactionType,
+    val amount: Double,
+    val balanceAfter: Double,
+    val timestamp: String
+)
 
 // This class represents a simple bank account
 class BankAccount(
     var accountHolder: String,
     var balance: Double)
 {
-    // List to store all transactions
-    private val transactionHistory = mutableListOf<String>()
+    // List to store all transaction objects
+    private val transactionHistory = mutableListOf<Transaction>()
+    private val dateFormat = SimpleDateFormat("HH:mm:ss", Locale.US)
 
     // Adds money to the account
     fun deposit(amount: Double){
-        balance += amount
-        transactionHistory.add("Deposited $${String.format(Locale.US, "%.2f", amount)}")
+        if (amount > 0) {
+            balance += amount
+            val transaction = Transaction(
+                TransactionType.DEPOSIT,
+                amount,
+                balance,
+                dateFormat.format(Date())
+            )
+            transactionHistory.add(transaction)
+        }
     }
 
     // Takes money out of the account if there are enough funds
     fun withdraw(amount: Double){
-        if(amount <= balance){
+        if (amount > 0 && amount <= balance) {
             balance -= amount
-            transactionHistory.add("Withdrew $${String.format(Locale.US, "%.2f", amount)}")
-        }else{
+            val transaction = Transaction(
+                TransactionType.WITHDRAWAL,
+                amount,
+                balance,
+                dateFormat.format(Date())
+            )
+            transactionHistory.add(transaction)
+        } else if (amount > balance) {
             println("You don't have the funds to withdraw $$amount")
         }
     }
@@ -30,12 +60,13 @@ class BankAccount(
     fun displayTransactionHistory(){
         println("Transaction history for $accountHolder")
         for(transaction in transactionHistory){
-            println(transaction)
+            val typeStr = if (transaction.type == TransactionType.DEPOSIT) "Deposited" else "Withdrew"
+            println("${transaction.timestamp}: $typeStr $${String.format(Locale.US, "%.2f", transaction.amount)} (Balance: $${String.format(Locale.US, "%.2f", transaction.balanceAfter)})")
         }
     }
 
     // Returns the list of transactions
-    fun getTransactionHistory(): List<String> {
+    fun getTransactionHistory(): List<Transaction> {
         return transactionHistory
     }
 }
